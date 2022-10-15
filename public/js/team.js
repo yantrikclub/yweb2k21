3eca6c39ac6050e9267333c1bdcf2d21dce4b540
Lakshay Nailwal <lakshaynailwal@gmail.com>
2022-10-15 21:06:06 +0530

    teamPage

--- public/js/team.js
+++ public/js/team.js
@@ -6,13 +6,13 @@
             let codei = ""
             let codel = ""
             let codef = ""
-            if (mobile != "javascript:void(0);"){
-                codem = ` <li style="--i:4">
-                <a href="${mobile}"  >
-                    <i class="fa fa-phone-square" aria-hidden="true"></i>
-                </a>
-            </li>`
-            }
+            // if (mobile != "javascript:void(0);"){
+            //     codem = `<li style="--i:4">
+            //     <a href="${mobile}"  >
+            //         <i class="fa fa-phone-square" aria-hidden="true"></i>
+            //     </a>
+            // </li>`
+            // }
             if (linkedin != "javascript:void(0);"){
                 codel = `<li style="--i:3">
                 <a target="_blank"  href="${linkedin}" >
